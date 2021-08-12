670271937dff60ce61eb82b407eba6a86c08d822
Ajay-droid-cmd <ajay.t5102001@gmail.com>
2021-08-12 14:40:21 +0530

    New Project structure

---
+++ src/matrixadd.js
@@ -0,0 +1,24 @@
+// To add two matrix
+function addMatrix(a, b) {
+  let result = new Array(a.length);
+  for (let i = 0; i < a.length; i++) {
+    result[i] = new Array(a[i].length);
+    for (let j = 0; j < result.length; j++) {
+      result[i][j] = a[i][j] + b[i][j];
+    }
+  }
+  return result;
+}
+
+let a = [
+  [4, 5, 6],
+  [7, 8, 9],
+  [10, 11, 12],
+];
+let b = [
+  [1, 2],
+  [4, 5],
+  [7, 8],
+];
+
+console.log(addMatrix(a, b));
