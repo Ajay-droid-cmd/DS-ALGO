33d192ad5db3dda60b6f6dd0395d1545e942a0a1
Ajay-droid-cmd <ajay.t5102001@gmail.com>
2021-08-14 22:34:09 +0530

    added matrix

--- src/magicmatrix.js
+++ src/magicmatrix.js
@@ -1,33 +1,80 @@
-// To find given matrix is a Magic Matrix 
-function isMagicMatrix(matrix,N) {
-    let sum1=0,sum2=0;
-    for (let i=0; i <N;i++){
-        sum1+=sum1+ matrix[i][i]
-        sum2+=sum2+ matrix[i][N-i-1]
-    } if(sum1 != sum2)
-        return false
-    
-        
-        for (let i= 0; i <N;i++){
-            let row=0,col=0;
-            for(let j=0; j <N;j++){
-                row += matrix[i][j]
-                
-                col += matrix[i][j]
-            }
-            if(row != col || col != sum1){
-                return false
-            }
+//To find weather the given matrix is a magic or not
+//function to find diagonal
+/*function digimat(mat) {
+  let n = mat.length;
+  var sum1 = 0;
+  var sum2 = 0;
+  for (let i = 0; i < n; i++) {
+    sum1 = sum1 + mat[i][i];
+    sum2 = sum2 + mat[i][n - 1 - i];
+  }
+  if (sum1 != sum2) 
+    return "Not a magic matrix";
+  
+
+  for (let i = 0; i < n; i++) {
+    let row = 0;
+    let col = 0;
+    for (let j = 0; j < n; j++) {
+      row += mat[i][j];
+      col += mat[j][i];
+
+      if (row != col || col != row) 
+        return "Not a col magic matrix";
+      
+    }
+  }
+  return " Its a match";
+}
+
+// Driver function
+let mat = [
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+];
+
+console.log(digimat(mat));*/
+// Returns true if mat[][] is magic
+// square, else returns false.
+function isMagicSquare(mat)
+{
+    var N = mat.length
+    // sumd1 and sumd2 are the sum of the two diagonals
+    var sumd1 = 0,sumd2=0;
+    for (var i = 0; i < N; i++)
+    {
+        // (i, i) is the diagonal from top-left -> bottom-right
+        // (i, N - i - 1) is the diagonal from top-right -> bottom-left
+        sumd1 = sumd1 + mat[i][i];
+        sumd2 = sumd2 + mat[i][N-1-i];
+    }
+    // if the two diagonal sums are unequal then it is not a magic square
+    if(sumd1!=sumd2)
+        return false;
+ 
+     
+    for (var i = 0; i < N; i++) {
+        var colSum = 0;
+        var rowSum = 0;   
+        for (var j = 0; j < N; j++)
+        {
+            rowSum += mat[i][j];
+            colSum += mat[j][i];
         }
-        return true;
+        if (rowSum != colSum || colSum != sumd1)
+            return false;
+    }
+    return true;
 }
-
-let matrix =[[ 2, 7, 1 ],
-            [ 9, 5, 1 ],
-            [ 4, 3, 8 ]];
-
-if(isMagicMatrix(matrix)){
-    console.log("Its Magic Matrix")
-} else {
-    console.log("its not magic matrix")
-}
+ 
+// driver program to
+// test above function
+var mat = [[ 1,2,3],
+           [ 1,2,3],
+           [ 1,2,3]];
+ 
+if (isMagicSquare(mat))
+    console.log( "Magic Square");
+else
+   console.log( "Not a magic Square");
